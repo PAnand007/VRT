ed0404699b4f3ed924ebcc75af14e4532f57d2f2
PAnand007 <143879416+PAnand007@users.noreply.github.com>
2023-12-27 19:05:43 +0530

    nothing

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>hello</h1>
+      <h1 class="bg-gray-100 text-left text-red-700 text-balance font-sans">hello</h1>
     </div>
   )
 }
